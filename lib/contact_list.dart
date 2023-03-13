7efe0b48f4ada8518f8836b06e6debb9ab0955b0
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-03-13 11:17:49 +0530

    added the contact list along with the notifications

--- lib/contact_list.dart
+++ lib/contact_list.dart
@@ -1,6 +1,11 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:its_urgent/home.dart';
+import 'package:its_urgent/sendNotifDialog.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ContactList extends StatefulWidget {
   const ContactList({super.key});
@@ -10,12 +15,88 @@ class ContactList extends StatefulWidget {
 }
 
 class _ContactListState extends State<ContactList> {
+
+  // defining two variables one is the contact list and the other is the boolean showing the loading state
+  List<Contact> contacts = [];
+  bool isLoading = true;
+  String? token;
+
+  // getting the permission for contacts
+  void getContacts() async {
+    if(await Permission.contacts.isGranted) {
+      fetchContacts();
+    }else{
+      print("this command ran");
+      await Permission.contacts.request();
+    }
+  }
+
+  // fetching the contacts
+  void fetchContacts() async{
+    contacts = await ContactsService.getContacts();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  
+  void getUserToken() {
+    OneSignal.shared.getDeviceState().then((deviceState) {
+      print(deviceState!.userId);
+      setState(() {
+        token = deviceState.userId;
+      });
+    });
+  }
+
+
+  // the init state for setting the new state
+  @override
+  void initState() {
+    init();
+    getUserToken();
+    getContacts();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(  
-      body: Center(  
-      child: Text("Contact List"),
+    return Scaffold(
+      appBar: AppBar(  
+        backgroundColor: Colors.grey[800],
+        elevation: 0,
+        leading: const Icon(Icons.menu),
+        title: const Text("Contacts"),
+        actions: const [
+          Icon(Icons.search),
+          SizedBox(width: 10),
+          Icon(Icons.more_vert),
+          SizedBox(width: 10),
+        ],
+      ),
+      body: Center( 
+        child: isLoading ?
+        const CircularProgressIndicator()
+        : ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index)=> GestureDetector(
+            onTap: ()=> showDialog(context: context, builder: (context)=>CustomDialog(token: token!, contact: contacts[index].displayName.toString())),
+            child: ListTile(
+              tileColor: Colors.grey[300],
+              leading: const Icon(Icons.person),
+              title: Text(contacts[index].displayName.toString()),
+            ),
+          ),
+        )
       ),
     );
   }
+}
+
+void init(){
+  OneSignal.shared.setAppId('684fdcc7-0b8c-439c-a654-84424caa4ebb');
+  OneSignal.shared.promptUserForPushNotificationPermission()
+    .then((accepted)=>{
+      print("Accepted permission: $accepted")
+    });
 }
---
+++ lib/sendNotifDialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:its_urgent/home.dart';
+
+class CustomDialog extends StatelessWidget {
+  String contact;
+  String token;
+  CustomDialog({required this.contact, required this.token, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(  
+      title: const Text("Send Notification"),
+      content: Text("Are you sure you want to send a notification to $contact?"),
+      actions: [
+        TextButton(onPressed: (){
+             sendNotif(token, "This is an urgent notification");
+              Navigator.pop(context);
+            }, 
+        child: const Text("Yes")),
+        TextButton(onPressed: (){
+          Navigator.pop(context);
+        }, child: const Text("No", style: TextStyle(color: Colors.grey),)),
+      ],
+    );
+  }
+}
