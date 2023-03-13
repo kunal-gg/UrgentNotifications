668e101785bca98b4b7bbbc0c01a2c185c2c3396
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-03-13 11:49:28 +0530

    implemented custom messages

--- lib/sendNotifDialog.dart
+++ lib/sendNotifDialog.dart
@@ -2,19 +2,39 @@
 import 'package:flutter/material.dart';
 import 'package:its_urgent/home.dart';
 
-class CustomDialog extends StatelessWidget {
+// ignore: must_be_immutable
+class CustomDialog extends StatefulWidget {
   String contact;
   String token;
   CustomDialog({required this.contact, required this.token, super.key});
 
   @override
+  State<CustomDialog> createState() => _CustomDialogState();
+}
+
+class _CustomDialogState extends State<CustomDialog> {
+  final TextEditingController _controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return AlertDialog(  
       title: const Text("Send Notification"),
-      content: Text("Are you sure you want to send a notification to $contact?"),
+      content: SizedBox(
+        height: 100,
+        child: Column(
+          children: [
+            Text("Are you sure you want to send a notification to ${widget.contact}?"),
+            TextField(  
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: "Enter your message here"
+              ),            
+            )
+          ],
+        ),
+      ),
       actions: [
         TextButton(onPressed: (){
-             sendNotif(token, "This is an urgent notification");
+             sendNotif(widget.token, _controller.text);
               Navigator.pop(context);
             }, 
         child: const Text("Yes")),
